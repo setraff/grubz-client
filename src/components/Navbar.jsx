9f8dedc03f41a1edc1807b1777e10539b1895a1d
Elgius <114131653+Elgius@users.noreply.github.com>
2023-12-03 15:37:02 +0500

    Fixed Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,8 +8,8 @@ const Navbar = () => {
   return (
     <div className='h-12 text-red-500 p-4 flex items-center justify-center border-b-2 border-b-red-500 uppercase md:h-24 lg:px-20 xl:px-40'>
       <div className='hidden md:flex gap-4 flex-1'>
-        <Link href='/'>Homepage</Link>
-        <Link href='/'>menu</Link>
+        <Link href='/'>Home</Link>
+        <Link href='/'>Menu</Link>
         <Link href='/'>Contact</Link>
       </div>
 
