cc980114cdf3283a24daab63316af665a665ec24
ra2-fayaz <rafhaan2.fayaz@live.uwe.ac.uk>
2023-12-20 00:30:11 +0500

    yuhijo

--- src/app/restaurants/[restaurant-code]/admin/Orders.jsx
+++ src/app/restaurants/[restaurant-code]/admin/Orders.jsx
@@ -1,7 +1,24 @@
+import api from '@/utils/api'
 import { Tab } from '@headlessui/react'
+import { useQuery, useQueryClient } from '@tanstack/react-query'
 import React from 'react'
 
 const Orders = () => {
+  const qc = useQueryClient()
+  const orderQuery = useQuery({
+    queryKey: ['orders'],
+    queryFn: () => api.get("/restaurants/orders").then(res => res.data)
+  })
+
+  const orders = orderQuery.data || []
+
+  const columns = [
+    { field: 'food', headerName: 'Food', width: 150, editable: false},
+    { field: 'description', headerName: 'Description', width: 150, editable: false},
+  ]
+
+  const rows = orders
+
   return (
     <Tab.Panel>
         <div>Orders</div>
