e5f79b0e0932566320731b6f5c05281caa9eada3
ra2-fayaz <rafhaan2.fayaz@live.uwe.ac.uk>
2023-12-21 19:24:55 +0500

    uoio

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,25 @@
+"use client"
+
 import Link from "next/link";
 import React from "react";
-import { Phone, ShoppingCart } from "lucide-react";
+import { LogOut, Phone, ShoppingCart } from "lucide-react";
 import MenuComp from "./Menu";
+import { useRouter } from "next/navigation";
 
 const Navbar = () => {
-  const user = false;
+  const user = localStorage.getItem('token') || null;
+  const restaurantCode = localStorage.getItem('restaurantCode')
+  const router = useRouter()
+  const handleLogOut = () => {
+    localStorage.removeItem('token')
+    router.push("/login")
+  }
+
   return (
     <div className="h-12 p-4 flex items-center justify-center border-b-2  uppercase md:h-24 lg:px-20 xl:px-40">
       <div className="hidden md:flex gap-4 flex-1">
-        <Link href="/">Home</Link>
-        <Link href="/menu">Menu</Link>
-        <Link href="/">Contact</Link>
+        <Link href={`/restaurants/${restaurantCode}/admin`}>Admin</Link>
+        <Link href="/menu">Orders</Link>
       </div>
 
       <div className="text-xl md:font-bold flex-1 md:text-center">
@@ -22,10 +31,7 @@ const Navbar = () => {
       </div>
 
       <div className="hidden md:flex gap-4 items-center justify-end flex-1">
-        {!user ? <Link href="/">Login</Link> : <Link href="/">Orders</Link>}
-        <Link href="/cart">
-          <ShoppingCart color="gray" />
-        </Link>
+        {!user ? <Link href="/login">Login</Link> : <LogOut onClick={handleLogOut} className="cursor-pointer"/>}
       </div>
     </div>
   );
